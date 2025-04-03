979c9bba9df894ad74963d0f35bce4c38c92ea8a
kvamca <kvamca@gmail.com>
2025-04-03 07:49:07 +0200

    game ui finished

--- javascript.js
+++ javascript.js
@@ -37,6 +37,13 @@ getHumanChoice();
 let humanScore = 0;
 let computerScore = 0;
 
+let currentScore = document.querySelector("#currentScore");
+let currentMoveHuman = document.querySelector("#currentMoveHuman");
+let currentMoveBot = document.querySelector("#currentMoveBot");
+let currentResult = document.querySelector("#currentResult");
+let winner = document.querySelector("#winner");
+
+let buttonReset = document.querySelector("#buttonReset")
 
 function playGame () {
     let computerChoice = getComputerChoice();
@@ -45,9 +52,12 @@ if (humanScore < 3 && computerScore < 3) {
     playRound(humanChoice, computerChoice);
 
     if (humanScore === 3) {
-        console.log("Human Wins!");
+        winner.textContent = ("Human Wins!");
+        buttonReset.innerHTML = ("<button>Reset Game</button>")
+
     } else if (computerScore === 3) {
-        console.log("Computer Wins!");
+        winner.textContent = ("Computer Wins!");
+        buttonReset.innerHTML = ("<button>Reset Game</button>")
     }
 }
 }
@@ -55,31 +65,51 @@ if (humanScore < 3 && computerScore < 3) {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === choices[0] && computerChoice === choices[2]) {
         humanScore = humanScore + 1;
-        console.log("Rock beats scissors: Human beats computer");
+        currentResult.textContent = ("Rock beats scissors: Human beats computer");
     } else if (humanChoice === choices[2] && computerChoice === choices[0]) {
         computerScore = computerScore + 1;
-        console.log("Rock beats scissors: Computer beats human")
+        currentResult.textContent = ("Rock beats scissors: Computer beats human")
     } else if (humanChoice === choices[2] && computerChoice === choices[1]) {
         humanScore = humanScore + 1;
-        console.log("Scissors beat paper: Human beats computer");
+        currentResult.textContent = ("Scissors beat paper: Human beats computer");
     } else if (humanChoice === choices[1] && computerChoice === choices[2]) {
         computerScore = computerScore + 1;
-        console.log("Scissors beat paper: Computer beats human")
+        currentResult.textContent = ("Scissors beat paper: Computer beats human")
     } else if (humanChoice === choices[1] && computerChoice === choices[0]) {
         humanScore = humanScore + 1;
-        console.log("Paper beats rock: Human beats computer");
+        currentResult.textContent = ("Paper beats rock: Human beats computer");
     } else if (humanChoice === choices[0] && computerChoice === choices[1]) {
         computerScore = computerScore + 1;
-        console.log("Paper beats rock: Computer beats human");
-    } else console.log("Tie!")
+        currentResult.textContent = ("Paper beats rock: Computer beats human");
+    } else currentResult.textContent = ("Tie!")
 
+    currentMoveBot.textContent = ("Computer plays: " + computerChoice);
+    currentMoveHuman.textContent = ("Human plays: " + humanChoice);
 
-    console.log("computer plays: " + computerChoice);
-    console.log("human plays: " + humanChoice);
+    currentScore.textContent = ("Current Score: " + "Human " + humanScore + " - Computer " + computerScore);
 
-    console.log("Current Score: Human " + humanScore + " - Computer " + computerScore);
-    console.log("______________");
+    // Reset humanChoice for the next selection
+    humanChoice = "";
+
+}
+
+buttonReset.addEventListener("click", (event) => {
+    resetGame();
+})
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    currentScore.textContent = "";
+    currentMoveHuman.textContent = "";
+    currentMoveBot.textContent = "";
+    currentResult.textContent = "";
+    winner.textContent = "";
+    buttonReset.innerHTML = "";
 
     humanChoice = "";
+    computerChoice = "";
+    randomNumber = "";
 
 }
